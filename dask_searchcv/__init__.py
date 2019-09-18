2a168745b22ff594cab7985a9685947045a6e1e1
enviz <48526315+enviz@users.noreply.github.com>
2019-09-18 09:17:14 +0530

    Update __init__.py
    But even after doing this it doesn't print the status bar. 
    Could you please fix that?

--- dask_searchcv/__init__.py
+++ dask_searchcv/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
-
-from .model_selection import GridSearchCV, RandomizedSearchCV
-
+#from .model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV,RandomizedSearchCV
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
